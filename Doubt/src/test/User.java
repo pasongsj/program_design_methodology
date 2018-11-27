e0be8519de9fb149b1775ffec6193237ab7d41cc
sujeong <audrbekf@gmail.com>
2018-11-28 00:18:17 +0900

    add_user

---
+++ Doubt/src/test/User.java
@@ -0,0 +1,10 @@
+package test;
+
+public class User 
+{
+	private Username D_VA[21], Hanzo, Genji, Soldier_76;
+	
+	
+	
+
+}
---
+++ Doubt/src/test/Username.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Username {
+	public void Ray(CardDeck cd) //Ã³À½ Ä«µå ³ª´²ÁÜ
+	{    for(int i=0;i<4;i++)	cd.getCard_count();
+	}
+
+}
