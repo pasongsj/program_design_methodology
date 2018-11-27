a288ef8d685eb31e8f231f609e6347b540418584
suelym Moon <msr56501354@gmail.com>
2018-11-28 00:17:11 +0900

    add username

---
+++ Doubt/src/test/User1.java
@@ -0,0 +1,12 @@
+package test;
+
+class user2{
+	private User1 D_VA,Hanzo,Genji,Solider_76;
+
+}
+public class User1 {
+	public static void main(String args[]) {
+		User1 un = new User1();
+	}
+	
+}
